cd5691203496ca6b9eee209456efb611d011f558
bigjun <zhijunliang777@gmail.com>
2019-09-05 22:39:33 +0800

    V4:multi-attention;introduce spatial_feat

--- model/config.py
+++ model/config.py
@@ -17,7 +17,7 @@ class CONFIGURATION(object):
         if feat_type=='fc7':
             if layer==1:
                 # # gnn node function
-                self.G_N_L_S = [1024*2, 1024, 1024]
+                self.G_N_L_S = [2144, 1024, 1024]
                 self.G_N_A   = ['ReLU', 'ReLU']
                 self.G_N_B   = bias
                 self.G_N_BN  = bn
@@ -37,6 +37,14 @@ class CONFIGURATION(object):
                 self.G_A_B   = bias
                 self.G_A_BN  = bn
                 self.G_A_D   = dropout
+
+                # gnn attention mechanism2
+                self.G_A_L_S2 = [176, 512, 1]
+                self.G_A_A2   = ['ReLU', 'LeakyReLU']
+                self.G_A_B2   = bias
+                self.G_A_BN2  = bn
+                self.G_A_D2   = dropout
+
             elif layer==2:
                 # # gnn node function
                 self.G_N_L_S = [1024*2, 1024, 1024]
--- model/grnn.py
+++ model/grnn.py
@@ -72,14 +72,25 @@ class O_NodeApplyModule(nn.Module):
         # return {'pred': pred}
         return {'new_n_f': n_feat}
 
-class E_AttentionModule(nn.Module):
+class E_AttentionModule1(nn.Module):
     def __init__(self, CONFIG):
-        super(E_AttentionModule, self).__init__()
-        self.attn_fc = MLP(CONFIG.G_A_L_S, CONFIG.G_A_A, CONFIG.G_A_B, CONFIG.G_E_BN, CONFIG.G_A_D)
+        super(E_AttentionModule1, self).__init__()
+        self.attn_fc = MLP(CONFIG.G_A_L_S, CONFIG.G_A_A, CONFIG.G_A_B, CONFIG.G_A_BN, CONFIG.G_A_D)
 
     def forward(self, edge):
         a_feat = self.attn_fc(edge.data['e_f'])
         return {'a_feat': a_feat}
+
+class E_AttentionModule2(nn.Module):
+    def __init__(self, CONFIG):
+        super(E_AttentionModule2, self).__init__()
+        self.attn_fc2 = MLP(CONFIG.G_A_L_S2, CONFIG.G_A_A2, CONFIG.G_A_B2, CONFIG.G_A_BN2, CONFIG.G_A_D2)
+
+    def forward(self, edge):
+        feat = torch.cat([edge.src['one_hot'], edge.data['s_f'], edge.dst['one_hot']], dim=1)
+        a_feat2 = self.attn_fc2(feat)
+
+        return {'a_feat2': a_feat2}
 
 class GNN(nn.Module):
     def __init__(self, CONFIG):
@@ -87,38 +98,45 @@ class GNN(nn.Module):
         self.apply_h_h_edge = H_H_EdgeApplyMoudle(CONFIG)
         self.apply_h_o_edge = H_O_EdgeApplyMoudle(CONFIG)
         self.apply_o_o_edge = O_O_EdgeApplyMoudle(CONFIG)
-        self.apply_edge_attn= E_AttentionModule(CONFIG)
+        self.apply_edge_attn1 = E_AttentionModule1(CONFIG)
+        self.apply_edge_attn2 = E_AttentionModule2(CONFIG)
         self.apply_h_node = H_NodeApplyModule(CONFIG)
         self.apply_o_node = O_NodeApplyModule(CONFIG)
 
     def _message_func(self, edges):
         # ipdb.set_trace()
-        return {'nei_n_f': edges.src['n_f'],  'a_feat': edges.data['a_feat']}
+        return {'nei_n_f': edges.src['n_f'], 'nei_one_hot': edges.src['one_hot'], 's_feat': edges.data['s_f'], 'a_feat': edges.data['a_feat'], 'a_feat2': edges.data['a_feat2']}
 
     def _reduce_func(self, nodes):
         # calculate the features of virtual nodes 
         # ipdb.set_trace()
-        alpha = F.softmax(nodes.mailbox['a_feat'], dim=1)
+        alpha1 = F.softmax(nodes.mailbox['a_feat'], dim=1)
+        alpha2 = F.softmax(nodes.mailbox['a_feat2'], dim=1)
+        alpha = (alpha1+alpha2)/2
         # z = torch.sum(torch.mul(alpha.repeat(1,1,1024), nodes.mailbox['nei_n_f']), dim=1).squeeze()
-        z = torch.sum( alpha * nodes.mailbox['nei_n_f'], dim=1)
+        z_raw_f = torch.cat([nodes.mailbox['nei_n_f'], nodes.mailbox['nei_one_hot'], nodes.mailbox['s_feat']], dim=2)
+        z_f = torch.sum( alpha * z_raw_f, dim=1)
         # when training batch_graph, here will process batch_graph graph by graph, 
         # we cannot return 'alpha' for the different dimension 
         if self.training or validation:
-            return {'z_f': z}
+            return {'z_f': z_f}
         else:
-            return {'z_f': z, 'alpha': alpha}
+            return {'z_f': z_f, 'alpha': alpha}
 
     def forward(self, g, h_node, o_node, h_h_e_list, o_o_e_list, h_o_e_list, pop_feat=False):
         
         if not len(h_h_e_list) == 0:
             g.apply_edges(self.apply_h_h_edge, tuple(zip(*h_h_e_list)))
+
         # ipdb.set_trace()
         if not len(o_o_e_list) == 0:
             g.apply_edges(self.apply_o_o_edge, tuple(zip(*o_o_e_list)))
         if not len(h_o_e_list) == 0:
             g.apply_edges(self.apply_h_o_edge, tuple(zip(*h_o_e_list)))
 
-        g.apply_edges(self.apply_edge_attn)
+        g.apply_edges(self.apply_edge_attn1)
+        g.apply_edges(self.apply_edge_attn2)
+
         g.update_all(self._message_func, self._reduce_func)
 
         if not len(h_node) == 0:
@@ -130,8 +148,11 @@ class GNN(nn.Module):
             # !NOTE:PAY ATTENTION WHEN ADDING MORE FEATURE
             g.ndata.pop('n_f')
             g.ndata.pop('z_f')
-            g.edata.pop('a_feat')
+            g.ndata.pop('one_hot')
+            g.edata.pop('s_f')
             g.edata.pop('e_f')
+            g.edata.pop('a_feat')
+            g.edata.pop('a_feat2')
             return g.ndata.pop('new_n_f')
         # # ipdb.set_trace()
         # if self.training or validation:
@@ -146,7 +167,7 @@ class GRNN(nn.Module):
 
     def forward(self, batch_graph, node_feat, spatial_feat, node_one_hot, batch_h_node_list, batch_obj_node_list, batch_h_h_e_list, batch_o_o_e_list, batch_h_o_e_list, valid=False, pop_feat=False):
         # !NOTE: if node_num==1, there is something wrong to forward the attention mechanism
-        ipdb.set_trace()
+        # ipdb.set_trace()
         global validation 
         validation = valid
 
